da9daa142c40665770f01cdde01e54d27ed4f463
Wageesh Arya <slowlearningcalligraphy@gmail.com>
2020-06-03 11:18:12 +0530

    production

---
+++ build/precache-manifest.875681b8f4efad283854c7050cbb649d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd14ddde8ad79a8d1d8bb05fb3cf9ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca0557e5522a9fde4c36",
+    "url": "/static/css/main.50af7b75.chunk.css"
+  },
+  {
+    "revision": "b9f0eaec19cb2f36b8a2",
+    "url": "/static/js/2.74340fd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74340fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0557e5522a9fde4c36",
+    "url": "/static/js/main.2d6dd659.chunk.js"
+  },
+  {
+    "revision": "9f1e78d60d8b52bd3d90",
+    "url": "/static/js/runtime-main.a86148e1.js"
+  },
+  {
+    "revision": "7c8257225b5b74c1f5bcea742eb69bbe",
+    "url": "/static/media/cornerplants.7c825722.svg"
+  },
+  {
+    "revision": "20fce5114b573804054c45b64f85cef5",
+    "url": "/static/media/cornerplants2.20fce511.svg"
+  },
+  {
+    "revision": "80aa6a780fc6c55ad2c7e3b0ec4038b6",
+    "url": "/static/media/cornerplants3.80aa6a78.svg"
+  },
+  {
+    "revision": "a5f84db2a3b5565c90dd8581f4e765a6",
+    "url": "/static/media/cornerplants4.a5f84db2.svg"
+  }
+]);
